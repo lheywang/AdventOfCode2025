8cb1bb824c0bad25d6d905437d2636eb5e6cb706
lheywang <leonard.heywang@proton.me>
2025-12-15 14:21:03 +0100

    Added day7 start of solution

--- day7/main.c
+++ day7/main.c
@@ -17,8 +17,14 @@
 
 // #define DEBUG
 
-#define ID_CNT 1000
-#define RANGE_CNT 179
+#define LINE_NB 142
+#define COLU_NB 141
+
+#define FREE (' ')
+#define USED ('o')
+#define SPLT ('^')
+
+static uint8_t array[LINE_NB][COLU_NB] = { 0 };
 
 int main(int argc, char **argv)
 {
@@ -26,25 +32,50 @@ int main(int argc, char **argv)
     FILE* file = fopen("input/input.txt", "r");
 
     // Initialize some data
-    char buffer[64] = {0};
-
-    // Read the file and compute some elements
-    bool mode = false;
-
-    int value_id = 0;
-    int range_id = 0;
+    char buffer[COLU_NB + 2] = {0};
+    int line = 0;
 
     if (file != NULL)
     {   
         while (fgets(buffer, sizeof(buffer), file))
         {
+            if (buffer[0] == '\n')
+                continue;
 
+            for (int chr = 0; chr < COLU_NB; chr ++)
+            {
+                switch (buffer[chr])
+                {
+                    case 'S':
+                        array[line][chr] = USED;
+                        break;
+                    case '^':
+                        array[line][chr] = SPLT;
+                        break;
+                    case '.':
+                    default:
+                        array[line][chr] = FREE;
+                        break;
+                }
+            }
+            line += 1;
+            // memset((void *)buffer, 0x00, (size_t)sizeof(buffer));
         }
         fclose(file);
     }
     else 
     {
         printf("Cannot open file !\n");
+    }
+
+    // Showing memory status
+    for (int i = 0; i < LINE_NB; i++)
+    {
+        for (int ii = 0; ii < COLU_NB; ii++)
+        {
+            printf("%c", array[i][ii]);
+        }
+        printf("\n");
     }
 
     return EXIT_SUCCESS;
